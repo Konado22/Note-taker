bd8e47a6a715644a77ebdb280f070d0f5582a1b2
unknown <jessedraper2@gmail.com>
2022-02-12 16:53:41 -0700

    fix file structure

--- server.js
+++ server.js
@@ -1,22 +1,20 @@
 const express = require("express");
-const app = express();
-const Store = require('./db/')
-const apiRoutes = require('./routes/apiRoutes')
-const htmlRoute = require('./routes/htmlRoutes')
+const htmlRoute = require("./routes/htmlRoutes");
+const apiRoute = require("./routes/apiRoutes");
+
 const PORT = process.env.PORT || 3001;
-//initilize 
-app.use('/', htmlRoute)
-app.use('/api',apiRoutes)
-app.use(express.static('public'));
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-//get request for homepage
-app.get('/', (req,res) => {
-res.sendFile((path.join(__dirname, '/public/index.html'))
-)})
-app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-})
-app.listen(PORT, ()=> {
-  console.log( `app listening at http://localhost:${PORT}`);
-})
+
+app.use(apiRoute);
+app.use(htmlRoute);
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
