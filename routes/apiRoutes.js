964c8dcb8b21616032b9c486b465961851cfe7f7
unknown <jessedraper2@gmail.com>
2022-02-12 17:30:11 -0700

    lots of progress

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,19 +1,24 @@
-const router = require('express').Router(); //using express
-const fs = require('fs');
-const path = require('path'); //__dirname? -- might not need
-var uniqid = require('uniqid'); //creates unique ID - change to const?
-const db = require('../db/db.json');
+const router = require("express").Router();
+var uniqid = require("uniqid");
+const { readFromFile, writeToFile } = require("../util/fs");
 
-router.get('/notes', () => {
-    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
-})
-router.post('/notes', (title, text) => {
+router.get("/notes", (req, res) => {
+  readFromFile("./db/db.json")
+    .then((data) => res.json(JSON.parse(data)))
+    .catch((err) => console.log(err));
+});
+router.post("/notes", (title, text) => {
+  router.post("/notes", (req, res) => {
+    const { title, text } = req.body;
+
     const newNote = {
-        title:this.title,
-        text:this.text,
-        id:uniqid()
-    }
-    readAndAppend(newNote, './db/db.json');
-    res.json("Updated");
-})
+      title,
+      text,
+      id: uniqid(),
+    };
+
+    readAndAppend(newNote, "./db/db.json");
+    res.json(newNote);
+  });
+});
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const express = require("express");
-const htmlRoute = require("./routes/htmlRoutes");
-const apiRoute = require("./routes/apiRoutes");
-
 const PORT = process.env.PORT || 3001;
-
+const routes = require('./routes/index')
 const app = express();
 
 app.use(express.static("public"));
@@ -12,8 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 
-app.use(apiRoute);
-app.use(htmlRoute);
+app.use(routes);
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
