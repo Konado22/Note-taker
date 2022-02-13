3c70b6ff258aee150c4f8c4035b3ac9d3c5b7f7c
unknown <jessedraper2@gmail.com>
2022-02-12 17:02:16 -0700

    routes looking better

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,9 +5,15 @@ var uniqid = require('uniqid'); //creates unique ID - change to const?
 const db = require('../db/db.json');
 
 router.get('/notes', () => {
-
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 })
-router.post('/notes', () => {
-
+router.post('/notes', (title, text) => {
+    const newNote = {
+        title:this.title,
+        text:this.text,
+        id:uniqid()
+    }
+    readAndAppend(newNote, './db/db.json');
+    res.json("Updated");
 })
 module.exports = router;
