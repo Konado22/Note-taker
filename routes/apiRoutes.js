1e81500e52c7e7bb45bdde0110b70b9104a02ad2
unknown <jessedraper2@gmail.com>
2022-02-12 19:07:52 -0700

    yay its working!

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,24 @@
 const router = require("express").Router();
 var uniqid = require("uniqid");
-const { readFromFile, writeToFile } = require("../util/fs");
+const { readFromFile, readAndAppend} = require("../util/fs");
 
 router.get("/notes", (req, res) => {
   readFromFile("./db/db.json")
     .then((data) => res.json(JSON.parse(data)))
     .catch((err) => console.log(err));
 });
-router.post("/notes", (title, text) => {
+
   router.post("/notes", (req, res) => {
-    const { title, text } = req.body;
+    const title = req.body.title
+    const body = req.body.body
 
     const newNote = {
-      title,
-      text,
+      title:title,
+      text: body,
       id: uniqid(),
     };
 
     readAndAppend(newNote, "./db/db.json");
     res.json(newNote);
   });
-});
 module.exports = router;
